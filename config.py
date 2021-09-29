25b23339698239fe511d99fcd2c656a5c4b6ec0e
PEBpung <mcuwwa29@gmail.com>
2021-09-29 15:24:01 +0900

    [Change] sweep config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 hyperparameter_defaults  = {
         'epochs': 2,
         'batch_size': 128,
+        'fc_layer_size': 128,
         'weight_decay': 0.0005,
         'learning_rate': 1e-3,
         'activation': 'relu',
@@ -16,7 +17,6 @@ hyperparameter_defaults  = {
 
 sweep_config = {
     'method': 'random',
-
     'metric' : {
         'name': 'loss',
         'goal': 'minimize'   
@@ -25,9 +25,6 @@ sweep_config = {
         'optimizer': {
             'values': ['adam', 'sgd']
             },
-        'fc_layer_size': {
-            'values': [128, 256, 512]
-            },
         'dropout': {
             'values': [0.3, 0.4, 0.5]
             },
@@ -35,19 +32,22 @@ sweep_config = {
             'value': [1, 2]
             },
         'learning_rate': {
-            # a flat distribution between 0 and 0.1
             'distribution': 'uniform',
             'min': 0,
             'max': 0.1
             },
         'batch_size': {
-            # integers between 32 and 256
-            # with evenly-distributed logarithms 
             'distribution': 'q_log_uniform',
             'q': 1,
             'min': math.log(32),
             'max': math.log(256),
             }
+        },
+    'early_terminate':{
+        'type': 'hyperband',
+        's': 2,
+        'eta': 3,
+        'max_iter': 27,
         }
     }
 
